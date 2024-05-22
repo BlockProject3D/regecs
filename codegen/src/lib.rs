c39982ee983395e947298ac7735dc7fe7e6b39f7
Yuri Edward <yuri6037@outlook.com>
2024-05-22 23:11:35 +0200

    Removed Object and New derive macros

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, BlockProject 3D
+// Copyright (c) 2024, BlockProject 3D
 //
 // All rights reserved.
 //
@@ -32,55 +32,11 @@ mod clear;
 mod dispatch;
 mod fields_enum;
 mod r#impl;
-mod new_impl;
-mod object_impl;
 
-use crate::new_impl::NewImpl;
-use crate::object_impl::ObjectImpl;
 use crate::r#impl::Impl;
 use clear::ClearImpl;
 use proc_macro::{self, TokenStream};
-use quote::ToTokens;
-use syn::{parse_macro_input, Attribute, DeriveInput, Type};
-
-fn get_context(attrs: impl Iterator<Item = Attribute>) -> Type {
-    attrs
-        .filter_map(|v| {
-            if v.path.clone().into_token_stream().to_string() == "context" {
-                Some(v.parse_args::<Type>().expect("failed to parse context"))
-            } else {
-                None
-            }
-        })
-        .last()
-        .expect("missing context")
-}
-
-#[proc_macro_derive(Object, attributes(context))]
-pub fn object(input: TokenStream) -> TokenStream {
-    let DeriveInput {
-        attrs, ident, data, ..
-    } = parse_macro_input!(input);
-    let context = get_context(attrs.into_iter());
-    ObjectImpl::parse_data((context, ident), data)
-        .into_token_stream()
-        .into()
-}
-
-#[proc_macro_derive(New, attributes(context))]
-pub fn new(input: TokenStream) -> TokenStream {
-    let DeriveInput {
-        attrs,
-        ident,
-        data,
-        vis,
-        ..
-    } = parse_macro_input!(input);
-    let context = get_context(attrs.into_iter());
-    NewImpl::parse_data((context, ident, vis), data)
-        .into_token_stream()
-        .into()
-}
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Clear, attributes(no_clear))]
 pub fn clear(input: TokenStream) -> TokenStream {
